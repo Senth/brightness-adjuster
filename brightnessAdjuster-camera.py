ea41ec5890530e8009f24386ebbcd885ece7c39a
senth <senth.wallace@gmail.com>
2016-02-23 19:39:41 +0100

    Camera brightness adjuster now changed behavior when the sun has set. It will decrease the brightness of the screen

--- brightnessAdjuster-camera.py
+++ brightnessAdjuster-camera.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python2
 import logging
+import ephem
 import cv2
-import re
 from datetime import datetime
 from time import gmtime, strftime, sleep
-from subprocess import call, Popen, check_output, CalledProcessError
+from subprocess import Popen, check_output, CalledProcessError
 
 
 # Always disabled adjustment and set it to default when any of these programs are running
@@ -12,29 +12,84 @@ PROGRAMS_DISABLED = ['mplayer', 'smplayer', 'vlc']
 # Always disabled adjustment and set it to default when anything is in fullscreen
 FULLSCREEN_DISABLED = True
 
-# Displays to change brightness of
+# Displays to change brightness of, check with ddccontrol -p
 DISPLAYS = ['adl:0:0', 'adl:0:1', 'adl:0:4']
+# XScreen, check with "echo $DISPLAY" in a terminal
+X_SCREEN = ':0.0'
 CAMERA = "/dev/video0"
 
 # Brightness adjustments depending on the sunset time.
-# First tuplet is minutes to sunset (can be negative)
-# Second tuplet is brightness in the range [0.0,1.0]
-ADJUST_MAX = 100
-ADJUST_MIN = 0
-ADJUST_MOVIE_MIN = 50
+BRIGHTNESS_MAX = 100
+BRIGHTNESS_MIN = 5
+BRIGHTNESS_WHEN_DARK_OUTSIDE = -10
+BRIGHTNESS_MOVIE_MIN = 50
 
+BRIGHTNESS_ADJUSTMENT_THRESHOLD_SUN_UP = 7
+BRIGHTNESS_ADJUSTMENT_THRESHOLD_SUN_DOWN = 3
 
-# --- Other non-essential settings ---
+# Location information
+LOCATION_LATITUDE = 55.7
+LOCATION_LONGITUDE = 13.2
+LOCATION_ELEVATION = 20
 
 # How long to wait between checking if something has changed, in seconds
 WAIT_TIME = 3
+# How many seconds to wait after adjusting the brightness before adjusting again
+WAIT_TIME_AFTER_ADJUSTMENT = 15
 
 LOG_LOCATION = '/tmp/brightnessAdjuster-camera.log'
-LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.DEBUG
 
 
 # Setup logging
 logging.basicConfig(format='%(asctime)s:%(levelname)s: %(message)s', filename=LOG_LOCATION, level=LOG_LEVEL, datefmt='%Y-%m-%d %H:%M:%S')
+
+class SunsetChecker:
+    def __init__(self):
+        self._location = ephem.Observer()
+        self._location.lat = str(LOCATION_LATITUDE)
+        self._location.lon = str(LOCATION_LONGITUDE)
+        self._location.elev = LOCATION_ELEVATION
+        self._location.pressure=1080
+        self._location.horizon='-6'
+        self.currentDate = 'invalid'
+        self._updateCurrentDate()
+        self._updateSunsetTime()
+
+    def update(self):
+        self._updateCurrentDate()
+        if not self._isSunsetTimeUpToDate():
+            self._updateSunset()
+
+    def _updateCurrentDate(self):
+        now = strftime("%Y-%m-%d", gmtime())
+        if now != self.currentDate:
+            self.currentDate = now
+            self.midnight = datetime.utcnow()
+            self.midnight.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    def _isSunsetTimeUpToDate(self):
+        if self.sunsetDate == self.currentDate:
+            return True
+        else:
+            return False
+
+    def _updateSunsetTime(self):
+        self.sunsetDate = self.currentDate
+        self._location.date = self.sunsetDate + " 12:00:00"
+        sunset = self._location.next_setting(ephem.Sun())
+        self.sunsetTime = sunset.datetime()
+
+    # @return positive if there are minutes left, negative if the sun has set
+    def getMinutesTillSunset(self):
+        now = datetime.utcnow()
+        secondsSinceMidnightNow = (now - self.midnight).total_seconds()
+        secondsSinceMidnightSunset = (self.sunsetTime - self.midnight).total_seconds()
+        return (secondsSinceMidnightSunset - secondsSinceMidnightNow) / 60
+
+    def isSunset(self):
+        return self.getMinutesTillSunset() <= 0
+
 
 class AmbientLightChecker:
     def __init__(self):
@@ -61,25 +116,23 @@ class AmbientLightChecker:
         return self.lux
 
     def getNormalizedLux(self):
-        return self.lux * 100 / 256
+        return float(self.lux) / float(256)
         
 
 class ProgramChecker:
-    DISPLAY=':0.0'
-
     def __init__(self, programsDisable, fullscreenDisable):
         self.programsDisable = programsDisable
         self.fullscreenDisable = fullscreenDisable
         self.fullscreenWindowIds = []
 
-    def checkForFullscreen(self):
+    def _checkForFullscreen(self):
         if self.fullscreenDisable:
             # Check old windows if they should be removed (as they aren't in fullscreen anymore
             for windowId in list(self.fullscreenWindowIds):
                 if not self._isWindowFullscreen(windowId):
                     self.fullscreenWindowIds.remove(windowId)
 
-            windowIdOutput = check_output(['xprop', '-display', self.DISPLAY, '-root', '_NET_ACTIVE_WINDOW']).decode('utf-8')
+            windowIdOutput = check_output(['xprop', '-display', X_SCREEN, '-root', '_NET_ACTIVE_WINDOW']).decode('utf-8')
             windowId = windowIdOutput[40:-1]
             logging.debug("Window id: " + windowId)
             if self._isWindowFullscreen(windowId):
@@ -90,13 +143,13 @@ class ProgramChecker:
 
     def _isWindowFullscreen(self, windowId):
         if len(windowId) == 9:
-            windowOutput = check_output(['xprop', '-display', self.DISPLAY, '-id', windowId]).decode('utf-8')
+            windowOutput = check_output(['xprop', '-display', X_SCREEN, '-id', windowId]).decode('utf-8')
             if windowOutput.find('_NET_WM_STATE_FULLSCREEN') != -1:
                 return True
             else:
                 return False
 
-    def isFullscreenActive(self):
+    def _isFullscreenActive(self):
         if len(self.fullscreenWindowIds) > 0:
             return True
         else:
@@ -104,18 +157,18 @@ class ProgramChecker:
 
     def shouldBeDisabled(self):
         for program in self.programsDisable:
-            if (self.isProgramRunning(program)):
+            if (self._isProgramRunning(program)):
                 return True
 
         # Check fullscreen
-        self.checkForFullscreen()
-        if self.isFullscreenActive():
+        self._checkForFullscreen()
+        if self._isFullscreenActive():
             logging.debug("Fullscreen is active")
             return True
 
         return False
 
-    def isProgramRunning(self, searchFor):
+    def _isProgramRunning(self, searchFor):
         output = check_output(['ps', 'aux']).decode('utf-8')
         if output.find(searchFor) != -1:
             logging.debug("Found program " + searchFor)
@@ -125,29 +178,41 @@ class ProgramChecker:
 
 
 class BrightnessAdjuster:
-    def __init__(self, adjustMin, adjustMax, adjustMovieMin):
-        self.adjustMin = adjustMin
-        self.adjustMax = adjustMax
-        self.adjustMovieMin = adjustMovieMin
+    def __init__(self):
         self.movieMode = False
         self.brightness = 0
-        
-    def setBrightness(self, brightness):
-        oldBrightness = self.brightness
-        if self.movieMode:
-            self.brightness = max(brightness, self.adjustMovieMin)
-        else:
-            self.brightness = brightness
 
-        diffBrightness = abs(oldBrightness - self.brightness)
-        if diffBrightness >= 5:
+    def setBrightness(self, lux, darkOutside):
+        brightness = int(lux * (BRIGHTNESS_MAX - BRIGHTNESS_MIN))
+        oldBrightness = self.brightness
+
+        if self.movieMode:
+            newBrightness = max(brightness, BRIGHTNESS_MOVIE_MIN)
+        elif darkOutside:
+            logging.debug("Decreasing brightness with " + str(BRIGHTNESS_WHEN_DARK_OUTSIDE) + " as it's dark outside")
+            newBrightness = min(100, max(0, brightness + BRIGHTNESS_WHEN_DARK_OUTSIDE))
+        else:
+            newBrightness = brightness
+
+        diffBrightness = abs(oldBrightness - newBrightness)
+        if darkOutside:
+            minDiff = BRIGHTNESS_ADJUSTMENT_THRESHOLD_SUN_DOWN
+        else:
+            minDiff = BRIGHTNESS_ADJUSTMENT_THRESHOLD_SUN_UP
+
+        if diffBrightness >= minDiff:
+            self.brightness = newBrightness
+            logging.info("Set brightness to " + str(self.brightness))
             for display in DISPLAYS:
                 Popen(['ddccontrol', '-r', '0x10', '-w', str(self.brightness), display])
+            sleep(WAIT_TIME_AFTER_ADJUSTMENT)
 
     def enableMovieMode(self):
+        logging.debug("Movie mode enabled")
         self.movieMode = True
 
     def disableMovieMode(self):
+        logging.debug("Movie mode disabled")
         self.movieMode = False
 
 
@@ -155,19 +220,21 @@ class BrightnessAdjuster:
 # Program Logic
 # -----------------------
 programChecker = ProgramChecker(PROGRAMS_DISABLED, FULLSCREEN_DISABLED)
-brightnessAdjuster = BrightnessAdjuster(ADJUST_MIN, ADJUST_MAX, ADJUST_MOVIE_MIN)
+brightnessAdjuster = BrightnessAdjuster()
 ambientLightChecker = AmbientLightChecker()
-
+sunsetChecker = SunsetChecker()
 
 while True:
     try:
         ambientLightChecker.update()
+        sunsetChecker.update()
+        darkOutside = sunsetChecker.isSunset()
         lux = ambientLightChecker.getNormalizedLux()
         if programChecker.shouldBeDisabled():
             brightnessAdjuster.enableMovieMode()
         else:
             brightnessAdjuster.disableMovieMode()
-        brightnessAdjuster.setBrightness(lux)
+        brightnessAdjuster.setBrightness(lux, darkOutside)
     except CalledProcessError:
         logging.warning("Couldn't call a subprocess")
     sleep(WAIT_TIME)
