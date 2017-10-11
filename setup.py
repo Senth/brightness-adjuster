684f9dd4cc6eb057b49e7437366209640651ec2e
senth <senth.wallace@gmail.com>
2017-10-11 12:09:37 +0200

    New directory structure and renamed project to whitelight

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Whitelight',
+      version='2.0',
+      description='Adjusts screen brightness and color',
+      author='Matteus Magnusson',
+      author_email='matteus.magnusson@gmail.com',
+      url='https://github.com/Senth/brightness-adjuster',
+      packages=['whitelight'],
+      )
